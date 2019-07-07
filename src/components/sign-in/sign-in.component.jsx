a2d95aa4cb87021791c8ac8bb873f56b9d4160f0
Pavel <Pawelv9@gmail.com>
2019-07-08 01:10:13 +0300

    Created ability to register with firebase database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import FormInput from '../form-input/form-input.component';
-import './sign-in.styles.scss'
 import CustomButton from '../custom-button/custom-button.component';
+import './sign-in.styles.scss'
 import { signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
