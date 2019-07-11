4bf98b21d36a4426f59fb2dbe8a9dc77d171868c
Pavel <Pawelv9@gmail.com>
2019-07-12 02:37:32 +0300

    Stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import keys from '../../keys'
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = keys.STRIPE_KEY;
+  
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+  
+  return (
+    <StripeCheckout 
+      label = 'Pay Now'
+      name = 'Great Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image = 'https://svgshare.com/i/CUz.svg'
+      description={`Your total price is $${price}`}
+      amount = {priceForStripe}
+      panelLabel = 'Pay Now'
+      token = {onToken}
+      stripeKey = {publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
