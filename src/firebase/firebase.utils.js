3d0c575fca0ce81baeb3016e86ee9d18d3dc92ce
Pavel <Pawelv9@gmail.com>
2019-07-12 19:33:33 +0300

    Deploy

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,7 +4,6 @@ import 'firebase/auth';
 import 'firebase/firestore';
 // import keys from '../keys'
 
-console.log(process.env.REACT_APP_API_KEY)
   const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
