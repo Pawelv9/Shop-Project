48345287295f9191c7565132cf67b5ebf65cae7b
Pavel <Pawelv9@gmail.com>
2019-07-14 03:33:03 +0300

    Added functions to receive data from firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -54,6 +54,20 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit()
 }
 
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollections = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+  console.log(transformedCollections)
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
