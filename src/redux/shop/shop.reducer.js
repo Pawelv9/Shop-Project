a01389db5df8545c24cbed50a3d1c5ae67630ed2
Pavel <Pawelv9@gmail.com>
2019-07-15 17:47:14 +0300

    adding async requests

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,17 +1,32 @@
-import SHOP_DATA from './shop.data'
+// import SHOP_DATA from './shop.data'
 import ShopActionTypes from './shop.types';
 
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA
+  collections: null,
+  isFetching: false,
+  errorMessage: undefined 
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ShopActionTypes.UPDATE_COLLECTIONS:
+    case ShopActionTypes.FETCH_CONNECTIONS_START: {
       return {
         ...state,
+        isFetching: true,
+      }
+    }
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
         collections: action.payload
+      }
+    case ShopActionTypes.FETCH_CONNECTIONS_FAILURE: 
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload
       }
     default:
       return state;
