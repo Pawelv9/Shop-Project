fcffa0dae83b94c41c793e8a678e06b22edbdbbb
Pavel <Pawelv9@gmail.com>
2019-07-17 04:39:52 +0300

    Refactoring Sign In with email and password with Redux-Saga

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component.jsx'
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 
@@ -19,7 +17,6 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
 
     // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     //   if (userAuth) {
@@ -61,9 +58,5 @@ class App extends React.Component {
     currentUser: selectCurrentUser
   })
 
-  const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser(user))
-  })
-
-  export default connect(mapStateToProps, mapDispatchToProps)(App);
+  export default connect(mapStateToProps)(App);
   
