3e43818ce880b6eeb6d5002b440e4b3e94ea97ae
Pavel <Pawelv9@gmail.com>
2019-07-30 02:06:28 +0300

    setting up a server

--- server/server.js
+++ server/server.js
@@ -2,3 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+if (process.env !== 'production') require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+if (process.env === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Spacecraft is launching on port' + port);
+})
