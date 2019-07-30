492aedab551b6b0556023153766c32c068a17048
Pavel <Pawelv9@gmail.com>
2019-07-30 03:06:22 +0300

    added backend payment route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 if (process.env !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_KEY);
+
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -24,4 +26,20 @@ if (process.env === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Spacecraft is launching on port' + port);
-})
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if(stripeErr) {
+      res.status(500).send({ error: stripeErr })
+    } else {
+      res.status(200).send({ success: stripeRes })
+    }
+  })
+});
