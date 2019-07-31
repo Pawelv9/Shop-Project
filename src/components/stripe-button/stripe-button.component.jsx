f5c16c62751ed0ac57d6cd1f8fc5a791eb8bdcfe
Pavel <Pawelv9@gmail.com>
2019-07-31 22:30:48 +0300

    removing backend part

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,7 +1,6 @@
 import 'dotenv/config'
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
 // import keys from '../../keys'
 
 const StripeCheckoutButton = ({ price }) => {
@@ -9,19 +8,8 @@ const StripeCheckoutButton = ({ price }) => {
   const publishableKey = process.env.REACT_APP_STRIPE_KEY;
   
   const onToken = token => {
-    axios({
-      url: 'payment',
-      method: 'post',
-      data: {
-        amount: priceForStripe,
-        token: token
-      }
-    }).then(response => {
-      alert('Payment successful')
-    }).catch(error => {
-      console.log('Payment error:', JSON.parse(error));
-      alert('There was an issue with your payment. Please sure you use the provided credit cart.');
-    });
+    console.log(token);
+    alert('Payment Successful');
   }
   
   return (
